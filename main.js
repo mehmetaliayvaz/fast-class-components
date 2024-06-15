534f71316af9693e9c056e3bed662418dd4b0cc9
Mehmet Ali Ayvaz <mehmetaliayvaz61@gmail.com>
2024-06-15 10:52:24 +0300

    Update accordion component

--- main.js
+++ main.js
@@ -1,33 +1,61 @@
 import "./style.css";
 
-const accordionButtons = document.querySelectorAll(".accordion-button");
+//döküman yüklenince 
 
-accordionButtons.forEach((accordionButton) => {
-  accordionButton.addEventListener("click", () => {
+document.addEventListener("DOMContentLoaded", function () {
+  const accordionButtons = document.querySelectorAll(".accordion-button");
 
-    const accordionGroup = accordionButton.parentElement.parentElement;
+  accordionButtons.forEach((accordionButton) => {
+    accordionButton.addEventListener("click", () => {
 
-    const contentHeight = accordionGroup.querySelector(".accordion-content").offsetHeight;
-    console.log(contentHeight)
+      const accordionGroup = accordionButton.parentElement.parentElement;
 
-    if (accordionGroup.classList.contains("accordion-group")) {
+      // if there is an accordion group
+      if (accordionGroup.classList.contains("accordion-group")) {
+        // if the clicked accordion button is already active
+        if (accordionButton.parentElement.classList.contains("active")) {
+          accordionGroup.querySelectorAll(".accordion").forEach((accordion) => {
+            accordion.classList.remove("active");
 
-      if (accordionButton.parentElement.classList.contains("active")) {
-        accordionGroup.querySelectorAll(".accordion").forEach((accordion) => {
-          accordion.classList.remove("active");
-        });
+            var content = accordion.querySelector(".accordion-content");
+            if (content?.style?.maxHeight) {
+              content.style.maxHeight = null;
+            }
+
+          });
+        }
+
+        // If the clicked accordion button is not active
+        else {
+          accordionGroup.querySelectorAll(".accordion").forEach((accordion) => {
+            accordion.classList.remove("active");
+
+            var content = accordion.querySelector(".accordion-content");
+            if (content?.style?.maxHeight) {
+              content.style.maxHeight = null;
+            }
+          });
+
+          accordionButton.parentElement.classList.add("active");
+
+          var panel = accordionButton.nextElementSibling;
+          panel.style.maxHeight = panel.scrollHeight + "px";
+        }
       }
+
+      // If there is no accordion group
       else {
-        accordionGroup.querySelectorAll(".accordion").forEach((accordion) => {
-          accordion.classList.remove("active");
-        });
+        accordionButton.parentElement.classList.toggle("active");
 
-        accordionButton.parentElement.classList.add("active");
+        var panel = accordionButton.nextElementSibling;
+
+        if (panel.style.maxHeight) {
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + "px";
+        }
       }
-    }
-    else {
-      accordionButton.parentElement.classList.toggle("active");
-    }
 
+    });
   });
 });
